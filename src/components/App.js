9b11353ff0bb42ebd88eb52f3b7deba2fe7dc0e3
Greyflanel <richardlovelace811@gmail.com>
2019-09-17 12:03:25 -0500

    Added reducers and actions folders with index.js files. Created actions and reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,8 @@
-import React from 'react';
+import React from "react";
 
-class App extends React.Component {
 
-    render() {
-        return <div>APPLICATION</div>
+const App = () => {
+  return <div>APPLICATION</div>;
 };
-    }
-    
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+return [
+    { title: 'No Scrubs', duration: '4:05' },
+    { title: 'Macarena', duration: '2:30' },
+    { title: 'All-Star', duration: '2:30' },
+    { title: 'I Want it That Way', duration: '3:15' },
+];   
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+}
+return selectedSong;
+}
