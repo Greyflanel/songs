9c6b17afa727eacdb96cc29253c09283fcdb4ec4
Greyflanel <richardlovelace811@gmail.com>
2019-09-21 20:39:55 -0500

    Songs is complete.

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const SongDetail = props => {
-    console.log(props);
+const SongDetail = ({ song }) => {
+    if (!song) {
+        return <div>Select A Song</div>
+    }
+    return  (
+    <div>
+        <h3>Details for:</h3>
+   <p>Title: {song.title} <br/>
+   Duration: {song.duration}</p> 
+    </div>
+        );
     
-    return <div>Song Detail</div>
 };
 
 const mapStateToProps = state => {
